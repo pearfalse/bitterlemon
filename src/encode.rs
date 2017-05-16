a3c7410cbb98e934022700ae138a512c59930218
Simon Harris <simon@pearfalse.com>
2017-05-16 22:16:19 +0100

    Unit tests, comments, minor renames

--- src/encode.rs
+++ src/encode.rs
@@ -1,91 +1,117 @@
 //! Handles encoding of a Bitterlemon image
 
-#[derive(Debug, Copy, Clone)]
-pub enum Rle {
-	SetRun(u8),
-	ClearRun(u8),
-	Frame,
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Run {
+	Set(u8),
+	Clear(u8)
+}
+
+impl Into<u8> for Run {
+	fn into(self) -> u8 {
+
+		use self::Run::*;
+
+		match self {
+			Set(n)   => {
+				assert!(n <= RLE_MAX_RUN);
+				let n_out = if n == RLE_MAX_RUN {0u8} else {n};
+				0xc0u8 + n_out
+			},
+			Clear(n) => {
+				assert!(n <= RLE_MAX_RUN);
+				let n_out = if n == RLE_MAX_RUN {0u8} else {n};
+				0x80u8 + n_out
+			},
+		}
+
+	}
 }
 
 #[derive(Debug)]
-pub struct RleState<S> {
-	state: Option<Rle>,
+pub struct RunIterator<S> {
+	state: Option<Run>,
 	source: S,
 }
 
-fn rle_new_run(pp: bool) -> Rle {
-	if pp {Rle::SetRun(1)} else {Rle::ClearRun(1)}
+fn rle_new_run(pp: bool) -> Run {
+	if pp {Run::Set(1)} else {Run::Clear(1)}
 }
 
 const RLE_MAX_RUN: u8 = 64;
 
-impl<S> RleState<S> {
+impl<S: Iterator<Item=bool>> RunIterator<S> {
 
-	pub fn from_pixels(source: S) -> RleState<S> {
-		RleState {
+	pub fn from_pixels(source: S) -> RunIterator<S> {
+		RunIterator {
 			state: None,
 			source: source
 		}
 	}
 }
 
-impl<S: Iterator<Item=bool>> Iterator for RleState<S> {
-	type Item = Rle;
+impl<S: Iterator<Item=bool>> Iterator for RunIterator<S> {
+	type Item = Run;
 
-	fn next(&mut self) -> Option<Rle> {
-		use self::Rle::*;
+	fn next(&mut self) -> Option<Run> {
+		use self::Run::*;
 		loop {
 
+			// Grab next bit from source; early return if None upstream
 			let pp = match self.source.next() {
 				Some(b) => b,
 				None    => return match self.state {
 					Some(x) => {
+						// Push out current state and clear for next time
 						let old = x;
 						self.state = None;
 						Some(old)
 					},
+					// All done
 					None => None
 				}
 			};
 
 			match self.state {
 
-				Some(SetRun(RLE_MAX_RUN)) => {
+				// Can't fit any more in this run
+				Some(Set(RLE_MAX_RUN)) => {
 					self.state = Some(rle_new_run(pp));
-					return Some(SetRun(RLE_MAX_RUN));
+					return Some(Set(RLE_MAX_RUN));
 				},
 
-				Some(ClearRun(RLE_MAX_RUN)) => {
+				// Can't fit any more in this run
+				Some(Clear(RLE_MAX_RUN)) => {
 					self.state = Some(rle_new_run(pp));
-					return Some(ClearRun(RLE_MAX_RUN));
+					return Some(Clear(RLE_MAX_RUN));
 				}
 
-				Some(SetRun(count)) if pp => {
-					self.state = Some(SetRun(count + 1));
+				// Increase existing Set run
+				Some(Set(count)) if pp => {
+					self.state = Some(Set(count + 1));
 					continue;
 				},
 
-				Some(SetRun(count)) if ! pp => {
+				// Swap from Set to Clear
+				Some(Set(_)) if ! pp => {
 					let old = self.state;
-					self.state = Some(ClearRun(1));
+					self.state = Some(Clear(1));
 					return old;
 				},
 
-				Some(ClearRun(count)) if ! pp => {
-					self.state = Some(ClearRun(count + 1));
+				// Increase existing Clear run
+				Some(Clear(count)) if ! pp => {
+					self.state = Some(Clear(count + 1));
 					continue;
 				},
 
-				Some(ClearRun(count)) if pp => {
+				// Swap from Clear to Set
+				Some(Clear(_)) if pp => {
 					let old = self.state;
-					self.state = Some(SetRun(1));
+					self.state = Some(Set(1));
 					return old;
 				},
 
-				Some(Frame) => {
-					unimplemented!();
-				}
-
+				// All cases covered, but can't convince rustc
 				Some(_) => {
 					unreachable!();
 				}
@@ -97,6 +123,47 @@ impl<S: Iterator<Item=bool>> Iterator for RleState<S> {
 			}
 
 		}
-		unreachable!();
+
 	}
 }
+
+#[cfg(test)]
+mod test_runs {
+
+	use std::iter::Iterator;
+
+	use super::{Run, RunIterator};
+	use super::Run::*;
+
+	#[test]
+	fn runs() {
+
+		let op = |input: &[u8], output: &[Run]| {
+
+			let bool_stream : Vec<bool> = input.iter().map(|&c| c == b'1').collect();
+			let collected : Vec<Run> = RunIterator::from_pixels(bool_stream.iter().map(|&b| b)).collect();
+
+			assert_eq!(output, collected.as_slice());
+		};
+
+		// simple single runs
+		op(b"1", &[Set(1)]);
+		op(b"1111", &[Set(4)]);
+		op(b"00", &[Clear(2)]);
+		op(b"", &[]);
+
+		// alterations
+		op(b"011", &[Clear(1), Set(2)]);
+		op(b"110", &[Set(2), Clear(1)]);
+		op(b"0101", &[Clear(1), Set(1), Clear(1), Set(1)]);
+		
+		// run size limits
+		let large_set = [b'1'; (super::RLE_MAX_RUN + 1) as usize];
+		op(&large_set, &[Set(super::RLE_MAX_RUN), Set(1)]);
+
+		// clear size limits
+		let large_clear = [b'0' ; (super::RLE_MAX_RUN + 1) as usize];
+		op(&large_clear, &[Clear(super::RLE_MAX_RUN), Clear(1)]);
+	}
+}
+
